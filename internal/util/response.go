443fdc4805c28f01326e013bee56b22e76139067
Firdaus Adi Putra <mail@firdaus.org>
2024-06-11 00:46:44 +0700

    Add WriteErrorf function

--- internal/util/response.go
+++ internal/util/response.go
@@ -19,7 +19,23 @@ func WriteJson(w http.ResponseWriter, code int, data interface{}) {
 	w.Write(response)
 }
 
-func WriteError(w http.ResponseWriter, code int, data error) {
+func WriteError(w http.ResponseWriter, code int, errMessage string) {
+	response, err := json.Marshal(map[string]interface{}{
+		"error": map[string]interface{}{
+			"message": errMessage,
+		},
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func WriteErrorf(w http.ResponseWriter, code int, data error) {
 	response, err := json.Marshal(map[string]interface{}{
 		"error": map[string]interface{}{
 			"message": data.Error(),
