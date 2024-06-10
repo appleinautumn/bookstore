fbc79826ec950da02972badff374c665eb43d4f6
Firdaus Adi Putra <mail@firdaus.org>
2024-06-10 14:42:36 +0700

    Init config

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"gotu/bookstore/internal/config"
 
 	"github.com/joho/godotenv"
 )
 
-func main() {
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
 
-	//  load env
+func main() {
+	// Load env
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	http.ListenAndServe(":8080", nil)
+	// Load configuration
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	log.Println(cfg)
+
+	http.HandleFunc("GET /", hello)
+	http.ListenAndServe(fmt.Sprintf(":%s", cfg.AppPort), nil)
 }
