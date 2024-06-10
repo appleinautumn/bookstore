7990d992a7827dd685a263dcf4b5358871b4042d
Firdaus Adi Putra <mail@firdaus.org>
2024-06-10 14:43:41 +0700

    Set logging level

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"gotu/bookstore/internal/config"
 
@@ -27,6 +29,22 @@ func main() {
 	}
 
 	log.Println(cfg)
+
+	// Set logging
+	if cfg.LogLevel == "debug" {
+		slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+			Level:     slog.LevelDebug,
+			AddSource: false,
+		})))
+	} else {
+		slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+			Level:     slog.LevelInfo,
+			AddSource: false,
+		})))
+	}
+	slog.Info("env loaded")
+	slog.Info("config loaded")
+	slog.Info("logging started")
 
 	http.HandleFunc("GET /", hello)
 	http.ListenAndServe(fmt.Sprintf(":%s", cfg.AppPort), nil)
