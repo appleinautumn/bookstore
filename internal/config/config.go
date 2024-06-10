b98d9fe404f76b5f53b42b9e03fcc8c94cb9aa12
Firdaus Adi Putra <mail@firdaus.org>
2024-06-10 14:37:34 +0700

    Add config

---
+++ internal/config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"errors"
+	"os"
+)
+
+type Config struct {
+	AppEnv     string
+	AppName    string
+	AppPort    string
+	AppVersion string
+}
+
+var configImpl *Config
+
+func New() (*Config, error) {
+
+	appEnv := getEnv("APP_ENV", "")
+	if appEnv == "" {
+		return nil, errors.New("APP_ENV env is required")
+	}
+
+	appName := getEnv("APP_NAME", "")
+	if appName == "" {
+		return nil, errors.New("APP_NAME env is required")
+	}
+
+	appPort := getEnv("APP_PORT", "")
+	if appPort == "" {
+		return nil, errors.New("APP_PORT env is required")
+	}
+
+	appVersion := getEnv("APP_VERSION", "")
+	if appVersion == "" {
+		return nil, errors.New("APP_VERSION env is required")
+	}
+
+	configImpl = &Config{
+		AppEnv:     appEnv,
+		AppName:    appName,
+		AppPort:    appPort,
+		AppVersion: appVersion,
+	}
+	return configImpl, nil
+}
+
+func GetConfig() (*Config, error) {
+	if configImpl == nil {
+		return nil, errors.New("config has not been initialized")
+	}
+
+	return configImpl, nil
+}
+
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultVal
+}
