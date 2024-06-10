89660f7dfde3e156114ed8bef5dd5456eabb8452
Firdaus Adi Putra <mail@firdaus.org>
2024-06-10 14:35:18 +0700

    Load env

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
 
+	//  load env
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	http.ListenAndServe(":8080", nil)
 }
