9329e7faa661f436e31a3adaf7bd634d61c456b2
Firdaus Adi Putra <mail@firdaus.org>
2024-06-11 00:44:40 +0700

    Add user repository

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"gotu/bookstore/internal/types"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+func (r *UserRepository) Create(ctx context.Context, u *types.User) (res *types.User, err error) {
+	sql := `INSERT INTO users (email, name, password)
+			VALUES ($1, $2, $3)
+			RETURNING id, email, name, created_at, updated_at`
+
+	row := r.db.QueryRow(sql, u.Email, u.Name, u.Password)
+
+	if err := row.Scan(
+		&u.ID,
+		&u.Email,
+		&u.Name,
+		&u.CreatedAt,
+		&u.UpdatedAt); err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
