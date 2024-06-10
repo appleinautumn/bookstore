fd9d376c4d11829b18838a697e971bc179e27c17
Firdaus Adi Putra <mail@firdaus.org>
2024-06-10 23:48:24 +0700

    Standarize API response

---
+++ internal/util/response.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func WriteJson(w http.ResponseWriter, code int, data interface{}) {
+	response, err := json.Marshal(map[string]interface{}{
+		"data": data,
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func WriteError(w http.ResponseWriter, code int, data error) {
+	response, err := json.Marshal(map[string]interface{}{
+		"error": map[string]interface{}{
+			"message": data.Error(),
+		},
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(code)
+	w.Write(response)
+}
