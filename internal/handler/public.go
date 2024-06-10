9bf66b0abd6d2078477c226e6266f33e4ece0ad8
Firdaus Adi Putra <mail@firdaus.org>
2024-06-11 00:47:55 +0700

    Add signup handler

--- internal/handler/public.go
+++ internal/handler/public.go
@@ -1,20 +1,24 @@
 package handler
 
 import (
+	"encoding/json"
 	"log/slog"
 	"net/http"
 
-	"gotu/bookstore/internal/book"
+	"gotu/bookstore/internal/request"
+	"gotu/bookstore/internal/service"
 	"gotu/bookstore/internal/util"
 )
 
 type ApiHandler struct {
-	service *book.BookService
+	bookService *service.BookService
+	userService *service.UserService
 }
 
-func NewApiHandler(sv *book.BookService) *ApiHandler {
+func NewApiHandler(sv1 *service.BookService, sv2 *service.UserService) *ApiHandler {
 	return &ApiHandler{
-		service: sv,
+		bookService: sv1,
+		userService: sv2,
 	}
 }
 
@@ -22,12 +26,40 @@ func (h *ApiHandler) ListBooks(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	// get books
-	books, err := h.service.List(ctx)
+	books, err := h.bookService.List(ctx)
 	if err != nil {
 		slog.Error("ListBooks", slog.Any("error", err))
-		util.WriteError(w, http.StatusInternalServerError, err)
+		util.WriteErrorf(w, http.StatusInternalServerError, err)
 		return
 	}
 
 	util.WriteJson(w, http.StatusOK, books)
+}
+
+func (h *ApiHandler) SignUp(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	// get payload
+	payload := &request.SignUpRequest{}
+	if err := json.NewDecoder(r.Body).Decode(payload); err != nil {
+		util.WriteError(w, http.StatusBadRequest, "Invalid request")
+		return
+	}
+
+	// validate
+	if errors := util.ValidateRequest(payload); len(errors) > 0 {
+		slog.Error("SignUp", slog.String("error", errors[0]))
+		util.WriteError(w, http.StatusBadRequest, errors[0])
+		return
+	}
+
+	// create
+	user, err := h.userService.Create(ctx, payload)
+	if err != nil {
+		slog.Error("SignUp", slog.Any("error", err))
+		util.WriteErrorf(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	util.WriteJson(w, http.StatusOK, user)
 }
