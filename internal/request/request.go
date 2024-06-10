f8653a9fc49330ed3a726ab34d65e82860dbc0d2
Firdaus Adi Putra <mail@firdaus.org>
2024-06-11 02:29:18 +0700

    Add OrderItem and OrderRequest

--- internal/request/request.go
+++ internal/request/request.go
@@ -5,3 +5,13 @@ type SignUpRequest struct {
 	Name     string `json:"name" validate:"required"`
 	Password string `json:"password" validate:"required"`
 }
+
+type OrderItem struct {
+	BookID   int `json:"book_id"`
+	Quantity int `json:"quantity"`
+}
+
+type OrderRequest struct {
+	Orders []OrderItem `json:"orders"`
+	UserID int64       `json:"user_id"`
+}
