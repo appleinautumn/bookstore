696d9fee4adf01ce1332bb793d43ae07d177784c
Firdaus Adi Putra <mail@firdaus.org>
2024-06-11 02:33:47 +0700

    Add createOrder and listOrders handler

--- internal/handler/public.go
+++ internal/handler/public.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
+	"strconv"
 
 	"gotu/bookstore/internal/request"
 	"gotu/bookstore/internal/service"
@@ -11,14 +12,16 @@ import (
 )
 
 type ApiHandler struct {
-	bookService *service.BookService
-	userService *service.UserService
+	bookService  *service.BookService
+	userService  *service.UserService
+	orderService *service.OrderService
 }
 
-func NewApiHandler(sv1 *service.BookService, sv2 *service.UserService) *ApiHandler {
+func NewApiHandler(sv1 *service.BookService, sv2 *service.UserService, sv3 *service.OrderService) *ApiHandler {
 	return &ApiHandler{
-		bookService: sv1,
-		userService: sv2,
+		bookService:  sv1,
+		userService:  sv2,
+		orderService: sv3,
 	}
 }
 
@@ -40,9 +43,9 @@ func (h *ApiHandler) SignUp(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	// get payload
-	payload := &request.SignUpRequest{}
-	if err := json.NewDecoder(r.Body).Decode(payload); err != nil {
-		util.WriteError(w, http.StatusBadRequest, "Invalid request")
+	var payload request.SignUpRequest
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		util.WriteErrorf(w, http.StatusBadRequest, err)
 		return
 	}
 
@@ -54,7 +57,7 @@ func (h *ApiHandler) SignUp(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// create
-	user, err := h.userService.Create(ctx, payload)
+	user, err := h.userService.CreateUser(ctx, &payload)
 	if err != nil {
 		slog.Error("SignUp", slog.Any("error", err))
 		util.WriteErrorf(w, http.StatusInternalServerError, err)
@@ -62,4 +65,56 @@ func (h *ApiHandler) SignUp(w http.ResponseWriter, r *http.Request) {
 	}
 
 	util.WriteJson(w, http.StatusOK, user)
+}
+
+func (h *ApiHandler) CreateOrder(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	// get payload
+	var payload request.OrderRequest
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		util.WriteErrorf(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// validate
+	if errors := util.ValidateRequest(payload); len(errors) > 0 {
+		slog.Error("SignUp", slog.String("error", errors[0]))
+		util.WriteError(w, http.StatusBadRequest, errors[0])
+		return
+	}
+
+	// create
+	order, err := h.orderService.CreateOrder(ctx, &payload)
+	if err != nil {
+		slog.Error("SignUp", slog.Any("error", err))
+		util.WriteErrorf(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	util.WriteJson(w, http.StatusOK, order)
+}
+
+func (h *ApiHandler) ListOrders(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	userIDHeader := r.Header.Get("user_id")
+
+	// convert user id to int64
+	userID, err := strconv.ParseInt(userIDHeader, 10, 64)
+	if err != nil {
+		slog.Error("ListOrders", slog.Any("error", err))
+		util.WriteErrorf(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// get orders
+	orders, err := h.orderService.ListOrdersByUserId(ctx, userID)
+	if err != nil {
+		slog.Error("ListOrders", slog.Any("error", err))
+		util.WriteErrorf(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	util.WriteJson(w, http.StatusOK, orders)
 }
