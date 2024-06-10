a4b859e7612d4d5e334b405d88087372a2dbdf0c
Firdaus Adi Putra <mail@firdaus.org>
2024-06-10 17:09:26 +0700

    Add Order and OrderBook struct

--- internal/types/entity.go
+++ internal/types/entity.go
@@ -14,6 +14,19 @@ type Book struct {
 	UpdatedAt   time.Time      `json:"updated_at"`
 }
 
+type Order struct {
+	ID        int64     `json:"id"`
+	UserID    int64     `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type OrderBook struct {
+	OrderID  int64 `json:"order_id"`
+	BookID   int64 `json:"book_id"`
+	Quantity int32 `json:"quantity"`
+}
+
 type User struct {
 	ID        int64     `json:"id"`
 	Email     string    `json:"email"`
