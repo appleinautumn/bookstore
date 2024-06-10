be0753538103ef8fa15c46a1616be4f4665408e3
Firdaus Adi Putra <mail@firdaus.org>
2024-06-10 15:52:39 +0700

    Update book struct

--- internal/types/entity.go
+++ internal/types/entity.go
@@ -1,7 +1,15 @@
 package types
 
+import (
+	"database/sql"
+	"time"
+)
+
 type Book struct {
-	ID     int64
-	Title  string
-	Author string
+	ID          int64          `json:"id"`
+	Title       string         `json:"title"`
+	Author      sql.NullString `json:"author"`
+	Description sql.NullString `json:"description"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
 }
