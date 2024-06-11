143ac1802791cbccca47a8f16bcc01e124a41f07
Firdaus Adi Putra <mail@firdaus.org>
2024-06-11 23:25:24 +0700

    Unit test ListOrdersByUserId

--- internal/repository/order_repository_test.go
+++ internal/repository/order_repository_test.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"context"
+	"fmt"
+	"math/rand/v2"
 	"testing"
 
 	"gotu/bookstore/internal/types"
@@ -9,6 +11,95 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-faker/faker/v4"
 )
+
+func TestListOrdersByUserId(t *testing.T) {
+	ctx := context.Background()
+
+	// mock db
+	db, mock := NewMock()
+	defer db.Close()
+
+	// create order repo
+	repo := NewOrderRepository(db)
+
+	// the userID who makes the request
+	userID := int64(rand.IntN(100))
+
+	// mock order 1
+	var o1 types.Order
+	if err := faker.FakeData(&o1); err != nil {
+		t.Errorf("err: %v", err)
+	}
+	o1.UserID = userID
+
+	// mock order 2
+	var o2 types.Order
+	if err := faker.FakeData(&o2); err != nil {
+		t.Errorf("err: %v", err)
+	}
+	o2.UserID = userID
+
+	// mock list of books
+	mockList := []*types.Order{
+		&o1,
+		&o2,
+	}
+
+	t.Run("success", func(t *testing.T) {
+		sql := `SELECT id, user_id, created_at, updated_at FROM orders WHERE user_id = \$1`
+
+		mockRows := sqlmock.NewRows([]string{"id", "user_id", "created_at", "updated_at"}).
+			AddRow(o1.ID, o1.UserID, o1.CreatedAt, o1.UpdatedAt).
+			AddRow(o2.ID, o2.UserID, o2.CreatedAt, o2.UpdatedAt)
+
+		mock.ExpectQuery(sql).WithArgs().WillReturnRows(mockRows)
+
+		// call ListOrdersByUserId
+		res, _ := repo.ListOrdersByUserId(ctx, userID)
+
+		// assert
+		if len(res) != len(mockList) {
+			t.Errorf("got: %d; want: %d", len(res), len(mockList))
+		}
+	})
+
+	t.Run("error - query", func(t *testing.T) {
+		sql := `SELECT id, user_id, created_at, updated_at FROM orders WHERE user_id = \$1`
+
+		mock.ExpectQuery(sql).WillReturnError(fmt.Errorf("some error"))
+
+		// call ListOrdersByUserId and throw error
+		if _, err := repo.ListOrdersByUserId(ctx, userID); err == nil {
+			t.Errorf("expecting an error, but there was none")
+		}
+
+		// make sure all expectations were met
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("unfulfilled expectations: %s", err)
+		}
+	})
+
+	t.Run("error - scan", func(t *testing.T) {
+		sql := `SELECT id, user_id, created_at, updated_at FROM orders WHERE user_id = \$1`
+
+		// "abc" is not a valid id
+		mockRows := sqlmock.NewRows([]string{"id", "user_id", "created_at", "updated_at"}).
+			AddRow("abc", o1.UserID, o1.CreatedAt, o1.UpdatedAt).
+			AddRow(o2.ID, o2.UserID, o2.CreatedAt, o2.UpdatedAt)
+
+		mock.ExpectQuery(sql).WillReturnRows(mockRows)
+
+		// call ListOrdersByUserId and throw error
+		if _, err := repo.ListOrdersByUserId(ctx, userID); err == nil {
+			t.Errorf("expecting an error, but there was none")
+		}
+
+		// make sure all expectations were met
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("unfulfilled expectations: %s", err)
+		}
+	})
+}
 
 func TestCreateOrder(t *testing.T) {
 	ctx := context.Background()
