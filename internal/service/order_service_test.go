54c4071eb132e13f67441b26eed4c025a92893f6
Firdaus Adi Putra <mail@firdaus.org>
2024-06-12 14:36:36 +0700

    Unit test CreateOrder

---
+++ internal/service/order_service_test.go
@@ -0,0 +1,137 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"math/rand/v2"
+	"reflect"
+	"testing"
+
+	"gotu/bookstore/internal/repository/mocks"
+	"gotu/bookstore/internal/request"
+	"gotu/bookstore/internal/types"
+
+	"github.com/go-faker/faker/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCreateOrder(t *testing.T) {
+	repository := mocks.NewOrderRepository(t)
+
+	t.Run("success", func(t *testing.T) {
+		// mock order item 1
+		var item1 request.OrderItem
+		if err := faker.FakeData(&item1); err != nil {
+			t.Errorf("err: %v", err)
+		}
+
+		// mock order item 2
+		var item2 request.OrderItem
+		if err := faker.FakeData(&item2); err != nil {
+			t.Errorf("err: %v", err)
+		}
+
+		// mock order request
+		orderReq := request.OrderRequest{
+			Orders: []*request.OrderItem{
+				&item1,
+				&item2,
+			},
+			UserID: int64(rand.IntN(100)),
+		}
+
+		// mock order1
+		var order1 types.Order
+		if err := faker.FakeData(&order1); err != nil {
+			t.Errorf("err: %v", err)
+		}
+
+		repository.On("CreateOrder", mock.Anything, mock.Anything).Return(&order1, nil).Once()
+		repository.On("CreateOrderItem", mock.Anything, mock.Anything).Return(nil)
+		service := NewOrderService(repository)
+
+		// create
+		res, err := service.CreateOrder(context.TODO(), &orderReq)
+
+		if !reflect.DeepEqual(&order1, res) {
+			t.Errorf("res and mockEntity are not deep equal")
+		}
+
+		// assert
+		assert.NoError(t, err)
+		assert.NotNil(t, res)
+	})
+
+	t.Run("error - empty orders", func(t *testing.T) {
+		// mock order request - empty order
+		orderReq := request.OrderRequest{
+			Orders: []*request.OrderItem{},
+			UserID: int64(rand.IntN(100)),
+		}
+
+		// mock order1
+		var order1 types.Order
+		if err := faker.FakeData(&order1); err != nil {
+			t.Errorf("err: %v", err)
+		}
+
+		service := NewOrderService(repository)
+
+		// create
+		res, err := service.CreateOrder(context.TODO(), &orderReq)
+
+		// assert
+		assert.Error(t, err)
+		assert.Nil(t, res)
+	})
+
+	t.Run("error - create order error", func(t *testing.T) {
+		// mock order request - empty order
+		orderReq := request.OrderRequest{
+			Orders: []*request.OrderItem{},
+			UserID: int64(rand.IntN(100)),
+		}
+
+		// mock order1
+		var order1 types.Order
+		if err := faker.FakeData(&order1); err != nil {
+			t.Errorf("err: %v", err)
+		}
+
+		repository.On("CreateOrder", mock.Anything, mock.Anything).Return(nil, errors.New("error"))
+		service := NewOrderService(repository)
+
+		// create
+		res, err := service.CreateOrder(context.TODO(), &orderReq)
+
+		// assert
+		assert.Error(t, err)
+		assert.Nil(t, res)
+	})
+
+	t.Run("error - create order item error", func(t *testing.T) {
+		// mock order request - empty order
+		orderReq := request.OrderRequest{
+			Orders: []*request.OrderItem{},
+			UserID: int64(rand.IntN(100)),
+		}
+
+		// mock order1
+		var order1 types.Order
+		if err := faker.FakeData(&order1); err != nil {
+			t.Errorf("err: %v", err)
+		}
+
+		repository.On("CreateOrder", mock.Anything, mock.Anything).Return(&order1, nil)
+		repository.On("CreateOrderItem", mock.Anything, mock.Anything).Return(nil, errors.New("error"))
+		service := NewOrderService(repository)
+
+		// create
+		res, err := service.CreateOrder(context.TODO(), &orderReq)
+
+		// assert
+		assert.Error(t, err)
+		assert.Nil(t, res)
+	})
+}
